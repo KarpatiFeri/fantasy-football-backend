29c773cd75bc716387d33f0f56daea6523a1adbb
KarpatiFeri <karpati.ferenc.93@gmail.com>
2021-12-17 09:38:50 +0100

    Add team service class

---
+++ src/main/java/com/fantasyfootball/service/TeamService.java
@@ -0,0 +1,24 @@
+package com.fantasyfootball.service;
+
+import java.util.List;
+
+import com.fantasyfootball.dao.TeamRepository;
+import com.fantasyfootball.model.Team;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TeamService {
+
+    private final TeamRepository teamRepository;
+
+    @Autowired
+    public TeamService(TeamRepository teamRepository) {
+        this.teamRepository = teamRepository;
+    }
+
+    public List<Team> getTeams() {
+        return teamRepository.findAll();
+    }
+}
