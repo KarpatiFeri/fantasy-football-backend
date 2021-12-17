b56cbde3f5d15f63f745e4930f639cc4c2e31bfe
KarpatiFeri <karpati.ferenc.93@gmail.com>
2021-12-17 09:38:14 +0100

    Add position enum

---
+++ src/main/java/com/fantasyfootball/model/player/Position.java
@@ -0,0 +1,19 @@
+package com.fantasyfootball.model.player;
+
+public enum Position {
+    GK,
+    LB,
+    LWB,
+    CB,
+    RWB,
+    RB,
+    LM,
+    CDM,
+    CM,
+    CAM,
+    RM,
+    LW,
+    CF,
+    ST,
+    RW
+}
