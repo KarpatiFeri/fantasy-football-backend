25fccbe413eb1d0f373c25db9ab5da1913ba5417
KarpatiFeri <karpati.ferenc.93@gmail.com>
2022-01-25 11:44:26 +0100

    make TeamService final

--- src/main/java/com/fantasyfootball/controller/TeamController.java
+++ src/main/java/com/fantasyfootball/controller/TeamController.java
@@ -2,19 +2,21 @@ package com.fantasyfootball.controller;
 
 import java.util.List;
 
-import com.fantasyfootball.model.Team;
+import com.fantasyfootball.model.team.Team;
 import com.fantasyfootball.service.TeamService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/fantasy-football")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TeamController {
 
-    private TeamService teamService;
+    private final TeamService teamService;
 
     @Autowired
     public TeamController(TeamService teamService) {
